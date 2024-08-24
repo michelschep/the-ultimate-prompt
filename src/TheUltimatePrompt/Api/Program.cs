d00f0ed5e8f1a7878d7a80df5205a4d65888d203
Michel Schep <michelschep@gmail.com>
2024-08-24 12:53:00 +0200

    Use api endpoint in blazor app

--- src/TheUltimatePrompt/Api/Program.cs
+++ src/TheUltimatePrompt/Api/Program.cs
@@ -1,5 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// The following line enables Application Insights telemetry collection.
+// We need to add configuration here to be sure the settings from the secrets file are used.
+// Somehow without passing in the configuration it only works when the connection string is in appsettings.
+builder.Services.AddApplicationInsightsTelemetry(builder.Configuration);
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -7,7 +12,19 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Cors is needed for Blazor client to call the api
+builder.Services.AddCors(policy =>
+{
+    policy.AddPolicy("CorsPolicy", opt => opt
+        .AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod());
+});
+
 var app = builder.Build();
+// Need to use CORS for Blazor client.
+// NOTE does order matter for the Use pipeline?
+app.UseCors("CorsPolicy");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,5 +38,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+var c = app.Services.GetService<IConfiguration>();
 app.Run();
